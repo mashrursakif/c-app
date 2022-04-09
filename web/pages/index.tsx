372ab13d4fb35afa968141b0b1dd80ebda9ee215
Mashrur Sakif <mashrursakifsouherdo@gmail.com>
2022-04-09 23:04:35 +0600

    Initial Commit

---
+++ web/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import React, { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import axios from "axios";
+
+const Home: NextPage = () => {
+	return (
+		<div>
+			<h1>Next.js</h1>
+		</div>
+	);
+};
+
+export default Home;
